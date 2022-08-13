a372642b30e2519139f076bfbb7750bdc96369b9
unknown <eloso85@gmail.com>
2022-08-13 12:22:30 -0500

    first commit

---
+++ static_site/index.js
@@ -0,0 +1,29 @@
+// function MainContent() {
+//     return (
+//         <div>
+//             <h1>My List</h1>
+//             <ul>
+//                 <li>dog</li>
+//                 <li>cat</li>
+//                 <li>pig</li>
+//             </ul>
+//         </div>
+//     )
+// }
+
+
+
+
+// ReactDOM.render(<MainContent />, document.getElementById("root"))
+
+// const element = <h1 className="header">This is an imperative way to program</h1>
+// console.log(element);
+
+// ReactDOM.render(element, document.getElementById("root"))
+
+const page = (
+    <div>
+        <h1 className="header">This is a header</h1>
+        <p>This is a paragragh</p>
+    </div>
+)
